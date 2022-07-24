dd94d35d4b260f39b5ca3ff6045822b87c79ed5a
SergeyK <en-karpov-serega@yandex.ru>
2022-07-24 23:40:54 +0300

    видимости

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	wordLine, _ := reader.ReadString('\n')
-	fmt.Println(GetCount(wordLine))
+	reader := bufio.NewScanner(os.Stdin)
+	reader.Scan()
+	//fmt.Print("Enter text: ")
+	wordLine := reader.Text()
+	line := strings.TrimSuffix(string(wordLine), "\n")
+	if line == "" {
+		fmt.Println(0)
+	} else {
+		fmt.Println(GetCount(line))
+	}
 }
